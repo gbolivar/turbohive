01296613a62873d11eccc98bce53e2d1512c2896
Gregorrio Bolivar <gregorio.bolivar@lcrcom.es>
2025-12-31 21:55:29 -0300

    gibfix the Bull Board

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,20 +1,8 @@
 import { Module } from '@nestjs/common';
 import { QueueModule } from './queue/queue.module';
 import { TasksModule } from './tasks/tasks.module';
-import { BullBoardModule } from './bull-board/bull-board.module';
 
 @Module({
-  imports: [QueueModule, TasksModule, BullBoardModule],
-  providers: [
-    {
-      provide: 'APP',
-      useFactory: async () => {
-        const app = await import('@nestjs/core').then(m => 
-          m.NestFactory.create(AppModule),
-        );
-        return app;
-      },
-    },
-  ],
+  imports: [QueueModule, TasksModule],
 })
 export class AppModule {}
